16999c991361ed100bb2c4af50cf7f638e02dbcc
KristinaGru <grudyte.kristina@gmail.com>
2024-02-02 21:39:25 +0100

    add header menu

--- portfolio/src/components/Header/Header.tsx
+++ portfolio/src/components/Header/Header.tsx
@@ -9,9 +9,14 @@ export default function Header() {
   return (
     <div style={{ backgroundColor }}>
       <div className={style.header}>
-        <Link to={'/'}>
-          <div className={style.content}>PORTFOLIO</div>
+        <Link to={'/'} className={style.main}>
+          PORTFOLIO
         </Link>
+        <div className={style.menu}>
+          <Link to={'/'}>home</Link>
+          <Link to={'/projects'}>projects</Link>
+          <Link to={'/contact'}>contact</Link>
+        </div>
       </div>
     </div>
   );
